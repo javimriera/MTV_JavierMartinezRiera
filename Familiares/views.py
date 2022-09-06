b542f9607166d67c5512ca5923ee28de3146e5be
Javier Martinez Riera <javimartinezriera@gmail.com>
2022-09-06 20:11:50 -0300

    Modificando el views

--- Familiares/views.py
+++ Familiares/views.py
@@ -1,11 +1,12 @@
+from django.shortcuts import render
 from django.template import Template, Context, loader
 from Familiares.models import Familia
 from django.http import HttpResponse
 
 
 def listar_familiares(request):
-    queryset = Familia.objects.all()
-    diccionario = {'Familiares': queryset}
+    queryset= Familia.objects.all()
+    diccionario= {'Familiares': queryset}
     plantilla= loader.get_template('familiares_list.html')
     documento_html = plantilla.render(diccionario)
 
