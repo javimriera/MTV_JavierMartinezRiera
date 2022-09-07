5014f2fc00a9ee6297eefd9e2b8fc2f40eb26cd0
Javier Martinez Riera <javimartinezriera@gmail.com>
2022-09-06 21:51:58 -0300

    Modificaciones varias

--- MTV_JMR/urls.py
+++ MTV_JMR/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from Familiares.views import listar_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Familiares/', listar_familiares)
+    path('Familiares/', include('Familiares.urls'))
 ]
